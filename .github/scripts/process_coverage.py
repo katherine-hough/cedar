cc209212e9f12a0dea29e8d9eaa9fe2b6bcf6411
Katherine Hough <kmhough@amazon.com>
2025-02-21 17:07:51 +0000

    * Started building the PR comment
    Signed-off-by: Katherine Hough <kmhough@amazon.com>

---
+++ .github/scripts/process_coverage.py
@@ -0,0 +1,50 @@
+import json
+import os
+import pathlib
+import sys
+import xml.etree.ElementTree as ET
+
+
+def collect_line_coverage(cobertura_file):
+    tree = ET.parse(cobertura_file)
+    root = tree.getroot()
+    entries = []
+    for package in root.find("packages"):
+        for clazz in package.find("classes"):
+            for line in clazz.find("lines"):
+                entry = dict(
+                    file_name=clazz.attrib["filename"],
+                    line_number=line.attrib["number"],
+                    hit_count=line.attrib["hits"],
+                )
+                entries.append(entry)
+    return entries
+
+
+def write_results(output_dir, passed, comment):
+    os.makedirs(pathlib.Path(output_dir).parent, exist_ok=True)
+    status_file = os.path.join(output_dir, "status.txt")
+    with open(status_file, "w") as f:
+        f.write("PASSED" if passed else "FAILED")
+    comment_file = os.path.join(output_dir, "markdown.md")
+    with open(comment_file, "r") as f:
+        f.write(comment)
+
+def read_json(file):
+    with open(file) as f:
+        return json.load(f)
+
+def process(
+    cobertura_file, changed_lines_file, threshold, report_location, output_dir
+):
+    coverage_entries = collect_line_coverage(cobertura_file)
+    changed_lines = read_json(changed_lines_file)
+    write_results(output_dir, False, changed_lines)
+
+
+def main():
+    process(*sys.argv[1:])
+
+
+if __name__ == "__main__":
+    main()
